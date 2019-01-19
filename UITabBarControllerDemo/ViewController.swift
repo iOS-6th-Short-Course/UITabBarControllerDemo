4cc2cddd19e1857f56afd4b7a5a4cee9841526d0
Chhaileng Peng <chhailengpeng@gmail.com>
2019-01-19 08:14:57 +0700

    Initial Commit

---
+++ UITabBarControllerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBarControllerDemo
+//
+//  Created by Chhaileng Peng on 1/19/19.
+//  Copyright © 2019 Chhaileng Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
